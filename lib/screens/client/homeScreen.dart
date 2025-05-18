5bfce16e6e942c8fc5a3b7ab40341c8a43196fbf
Phylip28 <felipe28769@gmail.com>
2025-05-17 21:26:19 -0500

    change background in homeScreen

--- lib/screens/client/homeScreen.dart
+++ lib/screens/client/homeScreen.dart
@@ -55,191 +55,189 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          // Imagen de fondo
-          Positioned.fill(child: Image.asset('assets/images/fondoCarga.png', fit: BoxFit.cover)),
-          SafeArea(
-            child: Column(
-              children: <Widget>[
-                // Recuadro blanco superior
-                Container(
-                  width: double.infinity,
-                  margin: const EdgeInsets.all(16.0),
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color.fromRGBO(0, 0, 0, 0.2),
-                        spreadRadius: 1,
-                        blurRadius: 3,
-                        offset: const Offset(0, 2),
-                      ),
-                    ],
+      backgroundColor: Colors.white, // Establecer el fondo blanco aquí
+      body: SafeArea(
+        // Se elimina el Stack y el Positioned.fill
+        child: Column(
+          children: <Widget>[
+            // Recuadro blanco superior
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromRGBO(0, 0, 0, 0.2),
+                    spreadRadius: 1,
+                    blurRadius: 3,
+                    offset: const Offset(0, 2),
                   ),
-                  child: Column(
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Deliver to', style: TextStyle(fontSize: 14, color: Colors.grey)),
+                  const SizedBox(height: 5),
+                  Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Text('Deliver to', style: TextStyle(fontSize: 14, color: Colors.grey)),
-                      const SizedBox(height: 5),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            'Your Location',
-                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          const Icon(Icons.arrow_drop_down),
-                        ],
+                      const Text(
+                        'Your Location',
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                       ),
-                      const SizedBox(height: 10),
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const LoginScreen()),
-                          );
-                        },
-                        child: const Text(
-                          'Register to get started',
-                          style: TextStyle(color: Color(0xFFf05000), fontSize: 12),
-                        ),
-                      ),
+                      const Icon(Icons.arrow_drop_down),
                     ],
                   ),
-                ),
-                Expanded(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Bloque de Categorías
-                        Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 16.0),
-                          padding: const EdgeInsets.all(16.0),
-                          decoration: BoxDecoration(
-                            color: Color.fromRGBO(255, 255, 255, 0.8),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Center(
-                                child: Text(
-                                  'Categories',
-                                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                              SizedBox(
-                                height: 120,
-                                child: ListView.builder(
-                                  scrollDirection: Axis.horizontal,
-                                  itemCount: _categories.length,
-                                  itemBuilder: (context, index) {
-                                    final category = _categories.elementAt(index);
-                                    return Padding(
-                                      padding: const EdgeInsets.only(right: 16.0),
-                                      child: Column(
-                                        children: [
-                                          Image.asset(category['image']!, height: 80),
-                                          const SizedBox(height: 8),
-                                          Text(
-                                            category['name']!,
-                                            style: const TextStyle(fontSize: 12),
-                                          ),
-                                        ],
-                                      ),
-                                    );
-                                  },
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        // Bloque de Restaurantes
-                        Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                          padding: const EdgeInsets.all(16.0),
-                          decoration: BoxDecoration(
-                            color: Color.fromRGBO(255, 255, 255, 0.8),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Center(
-                                child: Text(
-                                  'Restaurants',
-                                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                              SizedBox(
-                                height: 120,
-                                child: ListView.builder(
-                                  scrollDirection: Axis.horizontal,
-                                  itemCount: _restaurants.length,
-                                  itemBuilder: (context, index) {
-                                    final restaurant = _restaurants.elementAt(index);
-                                    return Padding(
-                                      padding: const EdgeInsets.only(right: 16.0),
-                                      child: Column(
-                                        children: [
-                                          InkWell(
-                                            onTap: () {
-                                              if (restaurant['name'] == 'Tierra Querida') {
-                                                Navigator.push(
-                                                  context,
-                                                  MaterialPageRoute(
-                                                    builder: (context) => const MenuTierraQuerida(),
-                                                  ),
-                                                );
-                                              } else if (restaurant['name'] == 'Starbucks') {
-                                                Navigator.push(
-                                                  context,
-                                                  MaterialPageRoute(
-                                                    builder:
-                                                        (context) =>
-                                                            const StarbucksMenuScreen(), // Navega a la nueva pantalla
-                                                  ),
-                                                );
-                                              } else {
-                                                print('Clicked on ${restaurant['name']}');
-                                              }
-                                            },
-                                            child: Column(
-                                              children: [
-                                                Image.asset(restaurant['image']!, height: 80),
-                                                const SizedBox(height: 8),
-                                                Text(
-                                                  restaurant['name']!,
-                                                  style: const TextStyle(fontSize: 12),
-                                                  textAlign: TextAlign.center,
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    );
-                                  },
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(height: 20), // Espacio al final del contenido
-                      ],
+                  const SizedBox(height: 10),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const LoginScreen()),
+                      );
+                    },
+                    child: const Text(
+                      'Register to get started',
+                      style: TextStyle(color: Color(0xFFf05000), fontSize: 12),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Bloque de Categorías
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                      padding: const EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(
+                          255,
+                          255,
+                          255,
+                          0.8,
+                        ), // Considera si este color aún es necesario o si debe ser blanco opaco
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Center(
+                            child: Text(
+                              'Categories',
+                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          SizedBox(
+                            height: 120,
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: _categories.length,
+                              itemBuilder: (context, index) {
+                                final category = _categories.elementAt(index);
+                                return Padding(
+                                  padding: const EdgeInsets.only(right: 16.0),
+                                  child: Column(
+                                    children: [
+                                      Image.asset(category['image']!, height: 80),
+                                      const SizedBox(height: 8),
+                                      Text(category['name']!, style: const TextStyle(fontSize: 12)),
+                                    ],
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Bloque de Restaurantes
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                      padding: const EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(255, 255, 255, 0.8),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Center(
+                            child: Text(
+                              'Restaurants',
+                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          SizedBox(
+                            height: 120,
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: _restaurants.length,
+                              itemBuilder: (context, index) {
+                                final restaurant = _restaurants.elementAt(index);
+                                return Padding(
+                                  padding: const EdgeInsets.only(right: 16.0),
+                                  child: Column(
+                                    children: [
+                                      InkWell(
+                                        onTap: () {
+                                          if (restaurant['name'] == 'Tierra Querida') {
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                builder: (context) => const MenuTierraQuerida(),
+                                              ),
+                                            );
+                                          } else if (restaurant['name'] == 'Starbucks') {
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                builder:
+                                                    (context) =>
+                                                        const StarbucksMenuScreen(), // Navega a la nueva pantalla
+                                              ),
+                                            );
+                                          } else {
+                                            print('Clicked on ${restaurant['name']}');
+                                          }
+                                        },
+                                        child: Column(
+                                          children: [
+                                            Image.asset(restaurant['image']!, height: 80),
+                                            const SizedBox(height: 8),
+                                            Text(
+                                              restaurant['name']!,
+                                              style: const TextStyle(fontSize: 12),
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 20), // Espacio al final del contenido
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: CustomBottomNavigationBar(
         currentIndex: _selectedIndex,
